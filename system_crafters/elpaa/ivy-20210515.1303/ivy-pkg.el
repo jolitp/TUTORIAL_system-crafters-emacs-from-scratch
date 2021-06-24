2fedd1c605cdcbbb04bd3dc0aacfb911a47b338a
João Luís <jolitp@gmail.com>
2021-06-24 18:49:47 -0300

    changes gitignore file

---
+++ system_crafters/elpaa/ivy-20210515.1303/ivy-pkg.el
@@ -0,0 +1,12 @@
+(define-package "ivy" "20210515.1303" "Incremental Vertical completYon"
+  '((emacs "24.5"))
+  :commit "e8ac2e23ffccb23600c5a3883805560927a54202" :authors
+  '(("Oleh Krehel" . "ohwoeowho@gmail.com"))
+  :maintainer
+  '("Oleh Krehel" . "ohwoeowho@gmail.com")
+  :keywords
+  '("matching")
+  :url "https://github.com/abo-abo/swiper")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ system_crafters/elpaa/visual-fill-0.1/visual-fill-autoloads.el
@@ -0,0 +1,27 @@
+;;; visual-fill-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "visual-fill" "visual-fill.el" (0 0 0 0))
+;;; Generated autoloads from visual-fill.el
+
+(autoload 'visual-fill-mode "visual-fill" "\
+Auto-refill paragraphs without modifying the buffer.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "visual-fill" '("visual-fill--")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; visual-fill-autoloads.el ends here
