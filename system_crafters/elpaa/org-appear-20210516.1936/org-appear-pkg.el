624b2b7160638fd4c363c1045774ba9e684a3114
João Luís <jolitp@gmail.com>
2021-06-24 18:51:10 -0300

    removed melpa folder

--- system_crafters/elpaa/org-appear-20210516.1936/org-appear-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-appear" "20210516.1936" "Auto-toggle Org elements" '((emacs "25.1") (org "9.3")) :commit "9fcf2486e70362d9fbdd40f3eae26ee0c04f13a9" :authors '(("Alice Istleyeva" . "awth13@gmail.com")) :maintainer '("Alice Istleyeva" . "awth13@gmail.com") :url "https://github.com/awth13/org-appear")
